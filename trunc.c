495de43c81045b7275ca048c1f9642b495d9af8b
Dave Beckett <dave@dajobe.org>
2009-06-13 14:49:42 -0700

    Add trunc.c using ftruncate()

---
+++ trunc.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+
+
+static size_t size = 1073741824;
+static size_t nitems = 10;
+
+
+#define USE_STDIO 0
+
+int main(int argc, char *argv[]);
+
+
+int main(int argc, char *argv[]) 
+{
+  char *filename;
+#if USE_STDIO
+  FILE *handle;
+#else
+  int fd;
+#endif
+  off_t buffer_size = (off_t)(size * nitems);
+  
+  filename=argv[1];
+  
+#if USE_STDIO
+  handle = fopen(filename, "wb");
+  if(!handle) {
+    fprintf(stderr, "Failed to open output file %s - %s\n", filename,
+            strerror(errno));
+    exit(1);
+  }
+#else
+  fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+  if(!fd) {
+    fprintf(stderr, "Failed to open output file %s - %s\n", filename,
+            strerror(errno));
+    exit(1);
+  }
+#endif
+
+
+#if USE_STDIO
+  error cannot
+#else
+  ftruncate(fd, buffer_size);
+#endif
+
+#if USE_STDIO
+  fclose(handle);
+#else
+  close(fd);
+#endif
+
+  return 0;
+}
