d5eb97f4df6ad3d3e3226448dbab34824f23e06d
Dave Beckett <dave@dajobe.org>
2025-04-06 16:25:43 -0700

    Use parse_size()

--- trunc.c
+++ trunc.c
@@ -1,7 +1,10 @@
 /*
  * trunc.c - make a lot of zeros
+ *
+ * USAGE:
+ *   trunc FILENAME SIZE[K|M|G]
  * 
- * Copyright (c) 2009 David Beckett
+ * Copyright (c) 2009-2024 David Beckett
  * 
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
@@ -31,10 +34,9 @@
 #include <fcntl.h>
 #include <string.h>
 #include <unistd.h>
+#include <ctype.h>
 
-
-static size_t size = 1073741824;
-static size_t nitems = 10;
+extern long long parse_size(const char *size_str, long long max_size);
 
 
 #define USE_STDIO 0
@@ -50,27 +52,42 @@ int main(int argc, char *argv[])
 #else
   int fd;
 #endif
-  off_t buffer_size = (off_t)(size * nitems);
-  
-  if(argc != 2) {
-    fprintf(stderr, "USAGE %s: FILENAME\n", argv[0]);
+  off_t target_size;
+  long long max_off_t;
+
+  /* Determine the maximum possible value that can be stored in a
+   * variable of type off_t
+   */
+  if((off_t)-1 > 0)
+    /* off_t is unsigned */
+    max_off_t = (long long)((off_t)-1);
+  else
+    /* off_t is signed. want max positive value. In binary: 011....111 */
+    max_off_t = (long long)(~((off_t)1 << (sizeof(off_t) * 8 - 1)));
+
+  if(argc != 3) {
+    fprintf(stderr, "USAGE: %s FILENAME SIZE[K|M|G|T|P]\n", argv[0]);
     exit(1);
   }
 
-  filename=argv[1];
+  filename = argv[1];
+  target_size = (off_t)parse_size(argv[2], max_off_t);
+  if (target_size < 0) {
+    exit(1);
+  }
   
 #if USE_STDIO
   handle = fopen(filename, "wb");
   if(!handle) {
-    fprintf(stderr, "Failed to open output file %s - %s\n", filename,
-            strerror(errno));
+    fprintf(stderr, "%s: Failed to open output file %s - %s\n",
+            argv[0], filename, strerror(errno));
     exit(1);
   }
 #else
   fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0644);
-  if(!fd) {
-    fprintf(stderr, "Failed to open output file %s - %s\n", filename,
-            strerror(errno));
+  if(fd < 0) {
+    fprintf(stderr, "%s: Failed to open output file %s - %s\n",
+            argv[0], filename, strerror(errno));
     exit(1);
   }
 #endif
@@ -79,7 +96,13 @@ int main(int argc, char *argv[])
 #if USE_STDIO
   error cannot
 #else
-  ftruncate(fd, buffer_size);
+  if(ftruncate(fd, target_size) == -1) {
+    fprintf(stderr, "%s: Failed to truncate file %s to %lld bytes - %s\n",
+            argv[0], filename, (long long)target_size, strerror(errno));
+    close(fd);
+  }
+  fprintf(stderr, "%s: Truncated %s to %lld bytes\n",
+          argv[0], filename, (long long)target_size);
 #endif
 
 #if USE_STDIO
--- zero.c
+++ zero.c
@@ -1,7 +1,15 @@
 /*
- * zero.c - make a lot of zeros
+ * zero.c - create a file containing zeros, optionally measuring write speed.
+ * Writes data in blocks, useful for testing filesystem/disk write performance.
+ *
+ * USAGE:
+ *   zero [OPTIONS] FILENAME
+ * OPTIONS
+ *   -b block_size : Size of each write block (default: 1G)
+ *   -t total_size : Total size of the file to create (default: block_size)
+ * Block size and total size are of form <integer>[KMGTP] eg 10M, 20P
  * 
- * Copyright (c) 2009 David Beckett
+ * Copyright (c) 2009-2024 David Beckett
  * 
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
@@ -33,20 +41,18 @@
 #include <string.h>
 #include <unistd.h>
 #include <inttypes.h>
+#include <ctype.h>
 
 #ifdef USE_MMAP
 #include <sys/mman.h>
 #else
 #endif
 
-/* static unsigned int K = 1024; */
-static unsigned int M = 1024*1024;
-static unsigned int G = 1024*1024*1024;
+extern long long parse_size(const char *size_str, long long max_size);
 
 
-static size_t size = 1073741824;
-static size_t nitems = 1;
-
+static const unsigned long long M = 1024*1024;
+static const unsigned long long G = 1024*1024*1024;
 
 int main(int argc, char *argv[]);
 
@@ -59,24 +65,65 @@ int main(int argc, char *argv[])
 #else
   int fd;
 #endif
-  size_t block_size = (size_t)(size * nitems);
+  size_t block_size = 1 * G; // Default block size: 1 GiB
+  size_t total_size = 0; // Default: write one block_size
+  size_t total_written = 0;
+  int opt;
 #ifdef USE_MMAP
   void *buffer = NULL;
 #else
   char *buffer = NULL;
 #endif
   double rate = 0.0;
-  int gigabytes = -1;
-  int gigabytes_count = -1;
-  
-  if(argc < 2 || argc > 3) {
-    fprintf(stderr, "USAGE %s: FILENAME [gigabytes count]\n", argv[0]);
-    exit(1);
+  int usage = 0;
+  /* size_t is always unsigned so the maximum possible value can be
+   * found from casting -1 */
+  long long max_size_t = (long long)((size_t)-1);
+
+  while ((opt = getopt(argc, argv, "b:ht:")) != -1) {
+    switch (opt) {
+      case 'b':
+        block_size = (size_t)parse_size(optarg, max_size_t);
+        if (!block_size)
+          exit(1);
+        break;
+
+      case 'h':
+        usage = 1;
+        break;
+
+      case 't':
+        total_size = (size_t)parse_size(optarg, max_size_t);
+        if (!total_size)
+          exit(1);
+        break;
+
+      default: /* '?' */
+        usage = 2;
+        break;
+    }
   }
 
-  filename=argv[1];
-  if(argc == 3)
-    gigabytes = atoi(argv[2]);
+  if (optind >= argc)
+    usage = 2;
+
+  if(usage > 0) {
+    fprintf(stderr,
+            "USAGE:\n"
+            "  %s [OPTIONS] FILENAME\n"
+            "OPTIONS\n"
+            "  -b block_size : Size of each write block (default: 1G)\n"
+            "  -t total_size : Total size of the file to create (default: block_size)\n"
+            "Block size and total size are of form <integer>[KMGTP] eg 10M, 20P\n",
+            argv[0]);
+    exit(usage > 1);
+  }
+
+  filename = argv[optind];
+
+  // If total size was not specified, default to writing one block
+  if (!total_size)
+    total_size = block_size;
 
 #ifdef USE_MMAP
   buffer = mmap(NULL /* addr */, block_size,
@@ -86,9 +133,9 @@ int main(int argc, char *argv[])
     fprintf(stderr, "Failed to mmap %" PRIuPTR " bytes - %s\n",
             block_size, strerror(errno));
     exit(1);
-  }
-  fprintf(stderr, "mmap()ed %" PRIuPTR " bytes at %p\n",
-          block_size, buffer);
+  } else
+    fprintf(stderr, "%s: mmap()ed %" PRIuPTR " bytes at %p\n",
+            argv[0], block_size, buffer);
 #else
   buffer = (char*)calloc(nitems, size);
   if(!buffer) {
@@ -107,28 +154,40 @@ int main(int argc, char *argv[])
   }
 #else
   fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0644);
-  if(!fd) {
+  if(fd < 0) {
     fprintf(stderr, "Failed to open output file %s - %s\n", filename,
             strerror(errno));
+#ifndef USE_MMAP
+    free(buffer);
+#endif
     exit(1);
   }
 #endif
 
-  if(gigabytes > 0)
-    gigabytes_count = gigabytes;
-
   while(1) {
-    size_t written;
+    ssize_t written_now; // write returns ssize_t
+    size_t expected_write;
     struct timeval tv;
     time_t start;
     suseconds_t start_usec;
     time_t end;
     suseconds_t end_usec;
-    size_t expected;
     double secs;
 
-    fprintf(stderr, "writing %" PRIuPTR " bytes to %s\n",
-            block_size, filename);
+    // Determine how much to write in this iteration
+    expected_write = block_size;
+    if (total_written + expected_write > total_size) {
+        expected_write = total_size - total_written;
+    }
+    if (expected_write == 0)
+      /* Should not happen if total_size >= block_size unless
+       * total_size was 0 initially */
+      break; 
+
+    fprintf(stderr,
+            "%s: Writing %" PRIuPTR " bytes to %s (%" PRIuPTR "/%" PRIuPTR " total)\n",
+            argv[0], expected_write, filename,
+            total_written + expected_write, total_size);
 
     gettimeofday(&tv, NULL);
     start = tv.tv_sec;
@@ -136,28 +195,41 @@ int main(int argc, char *argv[])
 
 #ifdef USE_STDIO
     expected = nitems;
-    written = fwrite(buffer, size , nitems, handle);
+    // THIS PATH IS NOT RECOMMENDED / TESTED WITH NEW LOGIC
+    // fwrite returns number of items successfully written.
+    // Need adaptation if USE_STDIO is truly desired.
+    fprintf(stderr, "USE_STDIO path needs review for size logic.\n");
+    written_now = fwrite(buffer, expected_write, 1, (FILE*)((void*)0)); // Deliberately broken
 #else
-    expected = block_size;
-    written = write(fd, buffer, block_size);
+    written_now = write(fd, buffer, expected_write);
 #endif
 
     gettimeofday(&tv, NULL);
     end = tv.tv_sec;
     end_usec = tv.tv_usec;
 
-    if(written != expected) {
-      fprintf(stderr, "WARNING write wrote %d items expected %d\n",
-              (int)written, (int)expected);
+    if(written_now < 0) {
+      fprintf(stderr,
+              "%s: ERROR writing to %s - %s\n",
+              argv[0], filename, strerror(errno));
+      break;
+    } else if ((size_t)written_now != expected_write) {
+      fprintf(stderr,
+              "%s: WARNING: partial write - wrote %zd bytes, expected %zu\n",
+              argv[0], written_now, expected_write);
+      total_written += written_now;
       break;
     }
-    secs = ((end-start) + (end_usec-start_usec)/1.0e6);
-    rate = block_size / secs;
-    fprintf(stderr,
-            "  wrote in %2.2f secs at %2.2f Mbytes/sec (%2.2f Gbytes/sec)\n",
-            secs, rate / M, rate / G);
 
-    if(gigabytes_count > 0 && !--gigabytes_count)
+    total_written += written_now;
+
+    secs = ((end-start) + (end_usec-start_usec)/1.0e6);
+    rate = (secs > 0) ? (written_now / secs) : 0;
+    fprintf(stderr,
+            "%s: Wrote in %2.2f secs at %2.2f Mbytes/sec (%2.2f Gbytes/sec)\n",
+            argv[0], secs, rate / M, rate / G);
+
+    if(total_written >= total_size)
         break;
     
   }
